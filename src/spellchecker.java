8c139d39f1c10f432ce137e0c878063ae278c7b7
ryan <ryanjones0911@gmail.com>
2023-03-05 15:51:25 -0600

    Just pushing this all out to github so i can access it on my other computers

---
+++ src/spellchecker.java
@@ -0,0 +1,55 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class spellchecker 
+{
+    private HashSet<String> words = new HashSet<>();
+
+    //we do this to make sure every instance of the spellchecker object has a new hashset with the words from words.txt initialized and available for use
+    public spellchecker()
+    {
+        this.words = new HashSet<>();
+        this.FillSet();
+    }
+
+    //this is a helper function used only to populate new instances of the spellchecker hashset with the words.txt wordlist
+    private void FillSet()
+    {
+        //try catch block to input content of words.txt to our hashset. Throws an error if the file cannot be found.
+        try 
+        {
+            File wordlist = new File("resources/words.txt"); 
+            Scanner iterate = new Scanner(wordlist);
+
+            //opens words.txt and adds every line as a new entry in the hashset
+            while(iterate.hasNextLine())
+            {
+                String data = iterate.nextLine();
+                words.add(data);
+            }
+
+            iterate.close();
+        } 
+        catch (FileNotFoundException e) 
+        {
+            System.err.println("Error: file not found");
+            System.exit(1);
+        }         
+    }
+
+    public boolean WordIsInSet(String word)
+    {
+        if(words.contains(word))
+        {
+            System.out.println("word found");
+            return true;
+        }
+        else
+        {
+            System.out.println("word not found");
+            return false;
+        }
+    }
+}
