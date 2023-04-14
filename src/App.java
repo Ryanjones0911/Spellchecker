73e23897618e2efc0b96cb7437cf28464db87695
ryan <ryanjones0911@gmail.com>
2023-04-14 09:55:10 -0500

    wrote singlecharinsert method

--- src/App.java
+++ src/App.java
@@ -5,6 +5,6 @@ public class App
 
         Spellchecker spellcheck = new Spellchecker();
 
-        spellcheck.Spellcheck("purpsoes");
+        spellcheck.Spellcheck("urposes");
     }
 }
--- src/spellchecker.java
+++ src/spellchecker.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
 
@@ -20,7 +22,7 @@ public class Spellchecker
         //try catch block to input content of words.txt to our hashset. Throws an error if the file cannot be found.
         try 
         {
-            File wordlist = new File("resources/words.txt"); 
+            File wordlist = new File("Spellchecker/resources/words.txt"); 
             Scanner iterate = new Scanner(wordlist);
 
             //opens words.txt and adds every line as a new entry in the hashset
@@ -45,43 +47,90 @@ public class Spellchecker
     //spellCheck(s) returns a list of every word in W that could be a correct spelling of s
     public void Spellcheck(String word)
     {
+        System.out.println("Now testing for direct match:");
+
         //call WordIsInSet to see if the word matches exactly with anything in the set. If there is no match...
         if(!WordIsInSet(word))
         {
             //...check if the given word is a match for any word in the wordlsit if adjacent characters
             //are swapped to account for potential mispellings
-
-            AdjacentSwap(word);
-
-            //..check if inserting a character in bewteen adjacent characters results in a match
-            SingleCharInsertion(word);
-
-            //..check if deleting a character in bewteen adjacent characters results in a match
-            SingleCharDelete(word);
-
-            //check if replacing a char in the word with another char results in a match
-            ReplaceChar(word);
+            System.out.println("now testing Adjacent Swap:");
+            if (!AdjacentSwap(word))
+            {
+                System.out.println("now testing Single Char Insertion:");
+                //..check if inserting a character in bewteen adjacent characters results in a match
+                if (!SingleCharInsertion(word))
+                {
+                    System.out.println("now testing Single Char Delete:");
+                    //..check if deleting a character in bewteen adjacent characters results in a match
+                    if(!SingleCharDelete(word))
+                    {
+                        System.out.println("now testing Replace Char:");
+                        //check if replacing a char in the word with another char results in a match
+                        if(!ReplaceChar(word))
+                        {
+                            System.out.println("No possible matches");
+                        }
+                    }
+                }
+            }
         }
     }
 
-    private void ReplaceChar(String word) 
+    private boolean ReplaceChar(String word) 
     {
         //iterate through char array and replace each letter with every other letter in the alphabet
         //and see if the resulting word results in a match
+        return false;
     }
 
-    private void SingleCharDelete(String word) 
+    private boolean SingleCharDelete(String word) 
     {
         //iterate through char array and delete each letter to see if the resulting word minus the deleted letter
         //results in a match
+        return false;
     }
 
-    private void SingleCharInsertion(String word)
+    private boolean SingleCharInsertion(String word)
     {
         //iterate through char array and basically just test for each character in the alphabet inserted between every letter
-        //to see if there's a match
-    }
+        //to see if there's a match. Time complexity O(n^2 * 26). The 26 comes from the characters of the alphabet
 
+        //this algo needs to insert characters at any given position in the char array.
+        //to do this means to push all entries in the array back to make room for a new
+        //array entry. This is not something that basic array easily supports
+        //hence the need to cast it to an arraylist
+        char[] charArray = word.toCharArray();
+        ArrayList<Character> characters = new ArrayList<>();
+        for(char c : charArray)
+        {
+            characters.add(c);
+        }
+       
+        for(int i = 0; i <= word.length(); i++)
+        {
+            for(char j = 'a'; j <= 'z'; j++)
+            {
+                characters.add(i, j);
+                System.out.println("Word being tested: " + characters.toString());
+
+                if(WordIsInSet(arrayListToString(characters)))
+                {
+                    return true;
+                }
+                else
+                {
+                    characters.clear();
+                    for(char c : charArray)
+                    {
+                        characters.add(c);
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
     private boolean AdjacentSwap(String word) 
     {
         //time complexity of this algo is O(n^2), and given the task it is performing I'm not sure it's possible to reduce it further.
@@ -124,4 +173,17 @@ public class Spellchecker
             return false;
         }
     }
+
+    private String arrayListToString(ArrayList<Character> list)
+    {
+        StringBuilder sb = new StringBuilder();
+        for(char c : list)
+        {
+            sb.append(c);
+        }
+        String result = sb.toString();
+        return result;
+    }
 }
+
+
